b26a88c99d31a78e5a83c26dcb323dcb0124b652
master <tailor.alm@gmail.com>
2019-03-15 16:02:43 -0300

    IMC - Pronto

--- app/src/main/java/br/edu/ifsc/myapplication/IMC.java
+++ app/src/main/java/br/edu/ifsc/myapplication/IMC.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
@@ -12,8 +13,11 @@ import java.text.DecimalFormat;
 public class IMC extends AppCompatActivity {
 
     private double imc;
+    private double imcCalculado;
     private double peso;
     private double altura;
+    private ImageView imagem;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,22 +34,54 @@ public class IMC extends AppCompatActivity {
         EditText editTextPeso = (EditText)findViewById(R.id.editTextPeso);
         peso = Double.parseDouble(editTextPeso.getText().toString());
 
+        ImageView imagem = (ImageView) findViewById(R.id.imageView);
+        imagem.setImageResource(R.drawable.perfil);
 
         Button button = (Button) findViewById(R.id.button);
-        calculaImc(altura, peso);
+        imcCalculado = calculaImc(altura, peso);
 
+        if (imcCalculado < 18.5) {
+            imagem.setImageResource(R.drawable.abaixopeso);
+            }
+            else if (imcCalculado > 18.6 && imcCalculado < 24.9){
+
+            imagem.setImageResource(R.drawable.normal);
+
+            }
+            else if (imcCalculado > 25 && imcCalculado < 29.9){
+
+                imagem.setImageResource(R.drawable.sobrepeso);
+
+            }
+            else if (imcCalculado > 30 && imcCalculado < 34.9){
+
+            imagem.setImageResource(R.drawable.obesidade1);
+
+            }
+            else if (imcCalculado > 35 && imcCalculado < 39.9){
+
+            imagem.setImageResource(R.drawable.obesidade2);
+
+            }
+            else if (imcCalculado > 40){
+
+            imagem.setImageResource(R.drawable.obesidade3);
+        }
+
+        DecimalFormat f = new DecimalFormat("#.##");
+        TextView text = (TextView) findViewById(R.id.imc);
+        text.setText(f.format(imcCalculado));
 
         }
 
         private Double calculaImc(double altura, double peso) {
 
         imc = peso/(altura*altura);
-        DecimalFormat f = new DecimalFormat("#.##");
 
-        TextView text = (TextView) findViewById(R.id.imc);
-        text.setText(Double.toString(imc));
+        //TextView text = (TextView) findViewById(R.id.imc);
+        //text.setText(Double.toString(imc));
 
-        return Double.parseDouble(f.format(imc));
+        return imc;
 
 
         }
